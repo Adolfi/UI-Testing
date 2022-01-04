1a60a85d826532534998f141a2a010d6d9428355
Dennis Adolfi <dennisadolfi@gmail.com>
2022-01-04 11:25:44 +0100

    added tag page tests

---
+++ Playwright/UI-Testing.PlaywrightTests/Tests/Tag/TagPageTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace UI_Testing.PlaywrightTests.Tests.Tag
+{
+    public class TagPageTests : PageTest
+    {
+        [Test]
+        public async Task Given_NavigateToUmbracoTags_Then_AssertLocation()
+        {
+            // When
+            await Page.GotoAsync("https://adolfi.dev/tags/umbraco");
+
+            // Then
+            Assert.AreEqual("https://adolfi.dev/tags/umbraco", Page.Url);
+        }
+
+        [Test]
+        public async Task Given_NavigateToUnitTestingTags_Then_AssertLocation()
+        {
+            // When
+            await Page.GotoAsync("https://adolfi.dev/tags/unit%20testing");
+
+            // Then
+            Assert.AreEqual("https://adolfi.dev/tags/unit%20testing", Page.Url);
+        }
+
+        [Test]
+        public async Task Given_NavigateToDotNetTags_Then_AssertLocation()
+        {
+            // When
+            await Page.GotoAsync("https://adolfi.dev/tags/-net");
+
+            // Then
+            Assert.AreEqual("https://adolfi.dev/tags/-net", Page.Url);
+        }
+    }
+}
